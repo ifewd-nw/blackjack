32faa0b1db4026faeae326b90cef27ba380497b8
Nathen Watters <wattersnathen@gmail.com>
2015-03-08 22:48:17 -0600

    Added original prototype functions back

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,34 @@
     }
 
     /**
+     * Get the value of the Card. If card is royal (not Ace)
+     * return 10 otherwise return the value
+     * @return {[type]} [description]
+     */
+    Card.prototype.getValue = function() {
+        return (this.value > 10) ? 10 : this.value;
+    };
+
+    /**
+     * Get the name (if royal card) or the value (if not royal card)
+     * @return {integer or string} representation of the card's value
+     */
+    Card.prototype.getName = function() {
+        if (this.value > 1 && this.value <= 10) {
+            return this.value;
+        } else {
+            switch(this.value) {
+                case 1:
+                    return 'A';
+                case 11:
+                    return 'J';
+                case 12:
+                    return 'Q';
+                case 13:
+                    return 'K';
+            }
+        }
+    };
      * Method that constructs the HTML for displaying the Card.
      * @return {jQuery object} formatted HTML string for the Card
      */
